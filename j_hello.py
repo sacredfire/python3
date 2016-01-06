1406390aa8732d64cc3fde021fcad08367fa086e
Sergei Kissel <sergei.kissel@gmail.com>
2016-01-06 13:58:45 +0200

    j_hello

---
+++ j_hello.py
@@ -0,0 +1,28 @@
+print("Hello Python3 world!")
+
+jane = 800
+boris = 380
+papa = 120
+mama = 1400
+total = jane + boris + papa + mama
+print(total)
+
+jane = 80
+papa = 120
+mama = 80
+boris = mama + papa
+total = jane + boris + papa + mama
+print(total)
+
+jane = 30
+papa = jane - 20
+mama = papa + 40
+boris = mama + papa
+total = jane + boris + papa + mama
+print(total)
+
+kolya = jane
+print(kolya)
+
+word = "apricot"
+print (word)
