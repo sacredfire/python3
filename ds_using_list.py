0c98f66b5b314ef9c5e5f486d4ff93b2d85f1e7b
Sergei Kissel <sergei.kissel@gmail.com>
2016-01-10 23:12:45 +0200

    data structures using tuples

---
+++ ds_using_list.py
@@ -0,0 +1,31 @@
+shoplist = ['apple','mango', 'potato', 'banana']
+
+print ('I have {} items to purchase'.format(len(shoplist)))
+
+def print_shoplist():
+	for item in shoplist:
+		print (item, end = " ") 
+		# This 'end = " "' part makes list in one line
+
+print ('\nThese items are:')
+print_shoplist()
+
+print ('\n\nI also need to buy beer..')
+shoplist.append('beer')
+
+print ('\nMy shoppinglist is now like this:')
+print_shoplist()
+
+print ('\n\nLet me sort my list now..')
+shoplist.sort()
+
+print ('\nMy sorted list is:')
+print_shoplist()
+
+print ('\n\nFirst I\'m gonna buy me {}s'.format(shoplist[0]))
+olditem = shoplist[0]
+del shoplist[0]
+print ('\nI bought me {}s.'.format(olditem))
+
+print ('\nMy shoppinglist is now like this:')
+print_shoplist()
---
+++ ds_using_tuples.py
@@ -0,0 +1,15 @@
+zoo = ('python', 'cat', 'mouse')
+
+print ('There are {} animals in the zoo.'.format(len(zoo)))
+
+new_zoo = ('tiger', 'pelican', zoo)
+
+print ('Number of cages in the new zoo is {}.'.format(len(new_zoo)))
+
+print ('All animals are {}.'.format(new_zoo))
+
+print ('Animals brought from old zoo are {}.'.format(new_zoo[2]))
+
+print ('Last animal brought from ols zoo is {}.'.format(new_zoo[2][2]))
+
+print ('Number of animals in the new zoo is {}.'.format(len(new_zoo)- 1 + len(zoo)))
