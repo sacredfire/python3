b24e4fe21f4d661d7ff4ce710ff57632baa765b3
Sergei Kissel <sergei.kissel@gmail.com>
2016-01-12 12:06:38 +0200

    lists_tuples

---
+++ lists_and_tuples.py
@@ -0,0 +1,13 @@
+tuple = (2, )
+print(tuple[0])
+
+list = [1, 2, 3, tuple]
+print(list)
+print(list[3])
+print(list[3][0])
+
+tuple1 = (2, 3, list, 4)
+print(tuple1)
+print(tuple1[2])
+print(tuple1[2][3])
+print(tuple1[2][3][0])
