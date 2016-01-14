a790afacf3299e372458fee8a70e272fe0e1761b
Sergei Kissel <sergei.kissel@gmail.com>
2016-01-14 17:36:36 +0200

    beg dect nested

---
+++ dict_nested.py
@@ -0,0 +1,16 @@
+d = {}
+d['dict1'] = {}
+d['dict1']['innerkay'] = 'value'
+print(d)
+
+# entry1 = {'name': 'Sergei',
+#           'surname': 'Kissel',
+#           'email': 'sergei.kissel@gmail.com',
+#           'tel': '+380919576297'
+#           }
+
+# entry2 = {'name': 'Dmytro',
+#           'surname': 'Kissel',
+#           'email': 'exz.mdma@gmail.com',
+#           'tel': '+‪16514347900‬'
+#           }
--- ds_using_dict.py
+++ ds_using_dict.py
@@ -1,22 +1,26 @@
-abook = {'Sergei Kissel': 'sk',
-         'Larry Page': 'lp',
-         'Dmytro Kissel': 'dk'
-         }
-
-sk = {'name': 'Sergei',
-      'surname': 'Kissel',
-      'email': 'sergei.kissel@gmail.com'
+ab = {'Swaroop': 'swaroop@gmail.com',
+      'Larry': 'larry@wall.com',
+      'Matrumoto': 'matz@ruby-lang.com',
+      'Spammer': 'spammer@spam.com'
       }
 
-lp = {'name': 'Larry',
-      'surname': 'Page',
-      'email': 'larry.page@gmail.com'
-      }
+print("Swaroop's address is", ab['Swaroop'])
 
-dk = {'name': 'Dmytro',
-      'surname': 'Kissel',
-      'email': 'dmytro.kissel@gmail.com'
-      }
+# Deleting a key-value pair
+del ab['Spammer']
 
-print("{}'s initials are {}".format(sk['name'], abook['Sergei Kissel']))
-print("{}'s initials are {}".format(lp['surname'], abook['Larry Page']))
+print('\nThere are {} contacts total in address book.\n'.format(len(ab)))
+
+for name, address in ab.items():
+    print('Contact {} at {}.'.format(name, address))
+
+# Adding a key-value pair
+ab['Guido'] = 'guido@python.org'
+
+if 'Guido' in ab:
+    print("\nGuido's address is", ab['Guido'])
+
+print('\nThere are {} contacts total in address book.\n'.format(len(ab)))
+
+for fck, fckrsaddress in ab.items():
+    print("Fuck's name is {} and his email is{}\n".format(fck, fckrsaddress))
