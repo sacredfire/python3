7d31b3662547739caa6ecd5f606aebc7f59a5b3d
Sergei Kissel <sergei.kissel@gmail.com>
2016-01-20 13:14:33 +0200

    dict nested success

--- dict_nested.py
+++ dict_nested.py
@@ -1,16 +1,28 @@
-d = {}
-d['dict1'] = {}
-d['dict1']['innerkey'] = 'value'
-print(d)
+# d = {}
+# d['dict1'] = {}
+# d['dict1']['innerkey'] = 'value'
+# print(d)
 
-# entry1 = {'name': 'Sergei',
-#           'surname': 'Kissel',
-#           'email': 'sergei.kissel@gmail.com',
-#           'tel': '+380919576297'
-#           }
+person1 = {'name': 'Sergei',
+           'surname': 'Kissel',
+           'email': 'sergei.kissel@gmail.com',
+           'tel': '+380919576297'
+           }
 
-# entry2 = {'name': 'Dmytro',
-#           'surname': 'Kissel',
-#           'email': 'exz.mdma@gmail.com',
-#           'tel': '+‪16514347900‬'
-#           }
+person2 = {'name': 'Dmytro',
+           'surname': 'Kissel',
+           'email': 'exz.mdma@gmail.com',
+           'tel': "+16514347900‬"
+           }
+
+params = {'me': person1,
+          'bro': person2
+          }
+
+print(params)
+
+print(params['me'])
+
+print('{} {}'.format(params['me']['name'], params['me']['surname']))
+
+print("My brother's tel num is {}".format(params['bro']['tel']))
