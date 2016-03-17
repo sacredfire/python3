dbcb5c5977083c6e0a11585028b7cc3d3d2e2d14
Sergei Kissel <sergei.kissel@gmail.com>
2016-03-17 15:35:31 +0200

    oop subclass finished

--- oop_subclass.py
+++ oop_subclass.py
@@ -4,8 +4,43 @@ class SchoolMember:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print('Initialized SchoolMember:'.format(self.name))
+        print('Initialized SchoolMember: {}'.format(self.name))
 
     def tell(self):
-        '''Tesubllls details'''
-        print
+        '''Tells details'''
+        print('Name: {} Age: {}'.format(self.name, self.age))
+
+
+class Teacher(SchoolMember):
+    '''Represents teacher'''
+
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+        print('Initialized Teacher: {}'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Salary: {:d}'.format(self.salary))
+
+
+class Student(SchoolMember):
+    '''Represents student'''
+
+    def __init__(self, name, age, grade):
+        SchoolMember.__init__(self, name, age)
+        self.grade = grade
+        print('Initialized Student: {}'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Grade: {:d}'.format(self.grade))
+
+t = Teacher('Mrs. Olivia Stoned', 40, 40000)
+s = Student('Michel Foo', 17, 5)
+
+print()
+
+members = [t, s]
+for member in members:
+    member.tell()
