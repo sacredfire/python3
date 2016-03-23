a467d16afa87bedd473e5e994aa3fa962328d393
sacredfire <sergei.kissel@gmail.com>
2016-03-23 18:57:15 +0200

    oop subclass python3

---
+++ oop_subclass_python3ver.py
@@ -0,0 +1,52 @@
+class SchoolMember(object):
+    '''Represents any school member'''
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print('Initialized SchoolMember: {}'.format(self.name))
+
+    def tell(self):
+        '''Tells details'''
+        print('Name: {} Age: {}'.format(self.name, self.age)),
+        # The trailing comma is used not to print new line
+
+
+class Teacher(SchoolMember):
+    '''Represents teacher'''
+
+    def __init__(self, name, age, salary):
+        #
+        #
+        #
+        super(Teacher, self).__init__(name, age)
+        #
+        #
+        self.salary = salary
+        print('Initialized Teacher: {}'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Salary: {:d}'.format(self.salary))
+
+
+class Student(SchoolMember):
+    '''Represents student'''
+
+    def __init__(self, name, age, grade):
+        SchoolMember.__init__(self, name, age)
+        self.grade = grade
+        print('Initialized Student: {}'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Grade: {:d}'.format(self.grade))
+
+t = Teacher('Mrs. Olivia Stoned', 40, 40000)
+s = Student('Michel Foo', 17, 5)
+
+print()
+
+members = [t, s]
+for member in members:
+    member.tell()
