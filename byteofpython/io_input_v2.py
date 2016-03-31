08d2cc77f55abdafb3d09519021e690a52301f6b
sacredfire <sergei.kissel@gmail.com>
2016-03-31 21:24:31 +0300

    io input v2 done!

--- byteofpython/io_input_v2.py
+++ byteofpython/io_input_v2.py
@@ -2,21 +2,24 @@ def cleanup(something):
     forbidden = ('!', '.', ',', '?', ':', ';', '-', '_', ' ', '...', '"', '\'')
     for char in forbidden:
         something = something.replace(char, "")
+        # something_clean_lowercase = something_clean.lower()
+    return(something.lower())
 
 
+def reverse(text):
+    return text[::-1]
 
 
-# def reverse(text):
-#     return text[::-1]
-
-
-# def is_palindrome(text):
-#     return text == reverse(text)
+def is_palindrome(text):
+    return text == reverse(text)
 
 something = str(input("Enter text: "))
 
+clean = cleanup(something)
 
-# if is_palindrome(something):
-#     print('Yes, it is a palindrome')
-# else:
-#     print('No, it is not a palindrome')
+print(clean)
+
+if is_palindrome(clean):
+    print('Yes, it is a palindrome')
+else:
+    print('No, it is not a palindrome')
