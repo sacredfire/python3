c4376de1a61d3c27239518b5c257fe09a683a6c1
sacredfire <sergei.kissel@gmail.com>
2016-04-15 09:43:29 +0300

    unicode

--- byteofpython/backup_v6.1.py
+++ byteofpython/backup_v6.1.py
@@ -9,6 +9,7 @@ target_dir = '/Users/Sergei_KIssel/python3/backup_DOCS'
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
 
+
 today = target_dir + os.sep + time.strftime('%Y%m%d')
 now = time.strftime('%H%M%S')
 comment = input('Enter comment here:')
