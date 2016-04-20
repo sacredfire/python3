724dcc14b5ac7dada12e6d82cbee304cb983a74c
sacredfire <sergei.kissel@gmail.com>
2016-04-20 19:14:07 +0300

    ..

--- byteofpython/unicode_utf.py
+++ byteofpython/unicode_utf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import io
 
-f = io.open("abc.txt", "wt", encoding="utf-8")
+f = io.open("abc.txt", "wt",)
 f.write(u"Здесь русский текст")
 f.close()
 
---
+++ crashcourse/bicycles.py
@@ -0,0 +1,12 @@
+bicycles = ['trek', 'cannondale', 'redline', 'hks']
+
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[1].title())
+
+print(bicycles[-1].upper())
+
+for val in bicycles:
+    print(val.title(), end=' ')
