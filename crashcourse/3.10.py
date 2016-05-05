a044e55f4c5e3c6c04c4a27b2d66f3331a1e602b
Sergei Kissel <sergei.kissel@gmail.com>
2016-05-05 09:54:11 +0300

    carsh magicians

--- crashcourse/3.10.py
+++ crashcourse/3.10.py
@@ -18,3 +18,32 @@ print(*tr)
 print(', '.join(str(t) for t in tr))
 
 print(len(trees))
+
+print(*trees)
+
+trees.reverse()
+print(*trees)
+
+trees.insert(4,'walnut')
+print(*trees)
+
+trees.remove('walnut')
+print(*trees)
+
+latin = 'acer'
+trees.remove(latin)
+print(*trees)
+
+poped = trees.pop(3)
+print(poped)
+print(*trees)
+
+poped1 = trees.pop(1)
+print(poped1)
+print(*trees)
+
+print(', '.join(str(t) for t in trees))
+print(len(trees))
+
+poped2 = trees.pop(2)
+print(poped2)
---
+++ crashcourse/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print('{} that was a trick!'.format(magician.title()))
+    print('Common, {}, show us more!\n'.format(magician.title()))
+print('\nThanks everyone! We\'re done here!')
